291a71dfab6dccf7e7541c68e06fcb1d367f51b1
김태형 <eddy@teamo2.kr>
2022-06-19 16:34:35 +0900

    [feature] main page ui 초기화 (feat. vuetify)

--- business_work_front/src/App.vue
+++ business_work_front/src/App.vue
@@ -1,60 +1,57 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
+  <v-app id="inspire">
+    <v-navigation-drawer
+        v-model="drawer"
+        fixed
+        app
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+      <v-list dense>
+        <v-list-item>
+          <v-list-item-action>
+            <i class="fas fa-home"></i>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-list-item-action>
+            <i class="fas fa-home"></i>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-card
+        color="grey lighten-4"
+        flat
+        tile>
+    <v-toolbar color="indigo" dark fixed>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Our Tasks</v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    </v-toolbar>
+    </v-card>
     <v-main>
-      <HelloWorld/>
+      <router-view></router-view>
     </v-main>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2022</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
-  name: 'App',
-
   components: {
-    HelloWorld,
   },
-
   data: () => ({
-    //
+    drawer : false
   }),
+  props: {
+    source: String
+  }
 };
 </script>
