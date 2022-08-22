2f5543f48d3c753109eebecb36ec186210d096da
김태형 <ktest92@gmail.com>
2022-08-23 00:23:55 +0900

    task 노출 설정

--- business_work_front/src/view/Project.vue
+++ business_work_front/src/view/Project.vue
@@ -9,7 +9,7 @@
   <v-card-title v-text="section.title"></v-card-title>
 <div class="overflow-y-auto" style="height:1000px;">
   <div>
-<v-col v-for="task in tasks" :key="task.title">
+<v-col v-for="task in section.taskList" :key="task.title">
     <!-- tasks -->
     <v-card>
       <v-checkbox v-model="todoCheck"></v-checkbox>
@@ -46,25 +46,7 @@ export default {
   data: () => ({
     todoCheck : false,
     tasks: [],
-      // { title: 'back office', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
-      // { title: 'erp product', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg', flex: 3 },
-      // { title: 'main application', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
-      // { title: 'main application1', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
-      // { title: 'main application2', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
-      // { title: 'main application3', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
-      // { title: 'main application4', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
-      // { title: 'main application5', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
-      // { title: 'main application6', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
-      // { title: 'main application7', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
-      // { title: 'main application8', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg', flex: 3 },
-    // ],
     sections: []
-      // {index:'1', title: 'todo'},
-      // {index:'2', title: 'in sprint'},
-      // {index:'3', title: 'PR'},
-      // {index:'4', title: 'PR'},
-      // {index:'5', title: 'PR'}
-    // ]
   }),
   created() {
     const store = this.$store;
@@ -76,7 +58,6 @@ export default {
 
     promise.then(function(_result) {
       console.log(_result);
-      console.log(this.$store.state.allData.sectionList);
     });
   }
 }
