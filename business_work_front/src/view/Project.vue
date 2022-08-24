0f35e2df7e0526787acaaf308d6c62fb674fe4b9
김태형 <ktest92@gmail.com>
2022-08-24 22:32:10 +0900

    task 이동 기능

--- business_work_front/src/view/Project.vue
+++ business_work_front/src/view/Project.vue
@@ -4,14 +4,14 @@
 <!-- sections -->
   <div v-for="section in this.$store.state.allData.sectionList" :key="section.index" class="m-2 section" 
   style="min-height:100%;position:relative;">
-  <v-card class="pb-15">
+  <v-card class="pb-15" @drop="drop($event)" @dragover="allowDrop($event)">
 
   <v-card-title v-text="section.title"></v-card-title>
 <div class="overflow-y-auto" style="height:1000px;">
   <div>
 <v-col v-for="task in section.taskList" :key="task.title">
     <!-- tasks -->
-    <v-card>
+    <v-card draggable="true" @dragstart="drag($event)" >
       <v-checkbox v-model="todoCheck"></v-checkbox>
       <v-card-title v-text="task.title"></v-card-title>
         <v-card-actions>
@@ -48,6 +48,20 @@ export default {
     tasks: [],
     sections: []
   }),
+  methods : {
+    drag(e) {
+      ev.dataTransfer.setData("text", ev.target.id);
+    },
+    drop(ev) {
+      console.log(ev);
+      ev.preventDefault();
+      var data = ev.dataTransfer.getData("text");
+      ev.target.appendChild(document.getElementById(data));
+    },
+    allowDrop(ev) {
+      ev.preventDefault();
+    }
+  },
   created() {
     const store = this.$store;
     const route = this.$route;
